945bce402a133de0f5c2b9d7272eccae4bff6538
SDim44 <68907465+SDim44@users.noreply.github.com>
2020-07-28 18:35:59 +0200

    Update Alf_Main.py

--- Alf_Main.py
+++ Alf_Main.py
@@ -70,7 +70,6 @@ try:
         #i+=1
         
         Mode = mode()
-        mod_changed(Mode)
 
  
         if Mode == "1":
@@ -78,7 +77,7 @@ try:
             print("\n{0}".format(pixy.get(1)))
             led.on(1,0,100,0)
             alf.hunt(1)
-            if mod_changed():
+            if mod_changed(Mode):
                 ask.say("Go to Object 1")
 
             
@@ -87,14 +86,15 @@ try:
             print("\n{0}".format(pixy.get(2)))
             led.on(1,100,0,0)
             alf.hunt(2)
-            if mod_changed():
+            if mod_changed(Mode):
                 ask.say("Back to Charging Station")
             
         elif Mode == "3": #Tool Mode
             print("Mode 3 - Toolmodus")
             led.on(1,0,0,100)
             engin.move(0,0,0,0)
-            ask.say("Tool Mode")                      
+            if mod_changed(Mode):    
+                ask.say("Tool Mode")                      
             
         elif Mode == "4": #Test Mode
             #messure = distance.get(1)
@@ -104,7 +104,7 @@ try:
             led.on(2,100,0,0)
         
         elif Mode == "5": #Face detection + follow
-            if mod_changed():
+            if mod_changed(Mode):
                 ask.say("Trying to find Humans")
 
 
