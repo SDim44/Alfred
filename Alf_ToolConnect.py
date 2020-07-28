60b59f76f20d92c70382ab35f9b564fabef1041e
SDim44 <68907465+SDim44@users.noreply.github.com>
2020-07-28 16:34:42 +0200

    Update Alf_ToolConnect.py

--- Alf_ToolConnect.py
+++ Alf_ToolConnect.py
@@ -23,15 +23,24 @@ logging.basicConfig(filename='/home/pi/Alfred/Logs/Alf_ToolConnect.log',level=lo
 logging.debug("----------- Starte Alfred Modul - Tool Connector {0} ---------------".format(Version))
 
 #Serielle Verbindung aufbauen
-ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
-ser.flush()
+#ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
+#ser.flush()
 
 
 
 #Funktion - Tool Ansteuern - Hier kann eine Funktion am Arduino ausgefuehrt werden. Ein webrequest wird gesendet - num ist die Nummer der Funktion. (Nach diesem Befehl muss ein do() ausgefuehrt werden)
 def set(command):
-    ser.write("{0}\n".format(command))
-    logging.debug("command wurde gesendet: {0}".format(command))
+    try:
+        ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
+        ser.flush()
+
+        ser.write("{0}\n".format(command))
+        logging.debug("command wurde gesendet: {0}".format(command))
+    except:
+        logging.debug("{0}\ncommand kann nicht gesendet werden (kein Tool erkannt)".format(command))
+
+
+    
 
 
 #Funktion - Tool Auslesen - Es wird die Zahl 99 and den Arduino gesendet, der Arduino sende daraufhin sein Spezifikationen. Diese werden hier ausgelesen und Zurueckgegeben.
