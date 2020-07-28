eb79e8ea9b2d96e3ae7d88bcb272def9deb71193
SDim44 <68907465+SDim44@users.noreply.github.com>
2020-07-28 17:36:10 +0200

    Create Alf_Speech.py

---
+++ Alf_Speech.py
@@ -0,0 +1,23 @@
+#
+#       Alfred Modul - Motorsteuerung
+#
+#       Stefan Dimnik, 14.03.2020
+#
+#       Dieses Programm ist für die Sprachausgabe zustaendig.
+
+#Libarys einbinden
+from subprocess import call
+import time
+import logging
+
+#Logging starten
+Version = "V1.0"
+logging.basicConfig(filename='/home/pi/Alfred/Logs/Alf_Speech.log',level=logging.DEBUG ,format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
+logging.debug("----------- Starte Alfred Modul - Sprachausgabe {0} ---------------".format(Version))
+
+
+#Text als Sprache ausgeben
+def say(speech):
+  
+    call(["espeak",speech])
+    logging.debug("Alfred: {0}".format(speech))
