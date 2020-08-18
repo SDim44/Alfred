d3b9aa1e5c9bcb6d659c9c0653717b79f499718d
SDim44 <68907465+SDim44@users.noreply.github.com>
2020-08-18 10:23:06 +0200

    Update Ultrasonictest.py

--- tests/Ultrasonictest.py
+++ tests/Ultrasonictest.py
@@ -1,5 +1,5 @@
 import Alf_Ultraschall as distance
-
+import time
 
 while True:
     DistanceOut = distance.get(1)
@@ -7,3 +7,5 @@ while True:
 
     DistanceOut = distance.get(2)
     print("Rueckgabewert Sensor {0} : {1}".format("2",DistanceOut))
+
+    time.sleep(1)
