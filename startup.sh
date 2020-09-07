621338ba182f5eeb5d31eb394c316860a46fe4a5
SDim44 <68907465+SDim44@users.noreply.github.com>
2020-09-07 11:45:56 +0200

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,3 +1,5 @@
 #Alfred starten
-
-python2.7 Alf_Main.py & python2.7 Alf_GUI.py & python2.7 Alf_Speech.py
+echo "99" > emotin.conf
+lxterminal --title="Alfred GUI" --working-directory="/home/pi/Alfred" --command bash -c "python2.7 Alf_GUI.py; bash"
+lxterminal --title="Alfred Speech" --working-directory="/home/pi/Alfred" --command bash -c "python2.7 Alf_Speech.py; bash"
+lxterminal --title="Alfred Main" --working-directory="/home/pi/Alfred" --command bash -c "sudo python2.7 Alf_Main.py; bash"
