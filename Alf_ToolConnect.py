35b82a078a1c4b30f94689e02f92b985d37127d6
SDim44 <68907465+SDim44@users.noreply.github.com>
2020-09-09 13:42:50 +0200

    Update Alf_ToolConnect.py

--- Alf_ToolConnect.py
+++ Alf_ToolConnect.py
@@ -21,7 +21,8 @@ import logging
 Version = "V0.2"
 logfile = 'logs/Alf_ToolConnect.log'
 logging.basicConfig(filename=logfile,level=logging.DEBUG ,format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
-logging.FileHandler(logfile, mode="w", encoding=None, delay=False)logging.info("----------- Starte Alfred Modul - Tool Connector {0} ---------------".format(Version))
+logging.FileHandler(logfile, mode="w", encoding=None, delay=False)
+logging.info("----------- Starte Alfred Modul - Tool Connector {0} ---------------".format(Version))
 
 #Serielle Verbindung aufbauen
 try:
