07742e9bd103b38c0ab18041dee22e5cc2071229
SDim44 <68907465+SDim44@users.noreply.github.com>
2020-09-09 16:01:54 +0200

    Update Alf_Start.sh

--- Alf_Start.sh
+++ Alf_Start.sh
@@ -1,4 +1,5 @@
 #Alfred starten
+sudo chmod +x /home/pi/Alfred
 echo "99" > emotin.conf
 lxterminal --title="Alfred GUI" --working-directory="/home/pi/Alfred" --command bash -c "python2.7 Alf_GUI.py; bash" &
 lxterminal --title="Alfred Speech" --working-directory="/home/pi/Alfred" --command bash -c "python2.7 Alf_Speech.py; bash" &
