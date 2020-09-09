269930ea67a885450e89ec8ccddf272792464170
SDim44 <68907465+SDim44@users.noreply.github.com>
2020-09-09 13:57:00 +0200

    Update Alf_Motor.py

--- Alf_Motor.py
+++ Alf_Motor.py
@@ -16,8 +16,8 @@ GPIO.setwarnings(False)
 
 #Logging starten
 Version = "V1.0"
-global logfile
-#logging.basicConfig(filename=logfile,level=logging.DEBUG ,format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
+logfile = 'logs/Alf_Main.log'
+logging.basicConfig(filename=logfile,level=logging.DEBUG ,format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
 #logging.FileHandler(logfile, mode="w", encoding=None, delay=False)
 logging.info("----------- Starte Alfred Modul - Motorsteuerung {0} ---------------".format(Version))
 
