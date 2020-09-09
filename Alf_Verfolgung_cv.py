dedaef95ca643790dd77cc7ba1ec54cb841571fc
SDim44 <68907465+SDim44@users.noreply.github.com>
2020-09-09 13:41:34 +0200

    Update Alf_Verfolgung_cv.py

--- Alf_Verfolgung_cv.py
+++ Alf_Verfolgung_cv.py
@@ -20,7 +20,7 @@ GPIO.setwarnings(False)
 
 #Logging config
 Version = "V1.0"
-lofile = 'logs/Alf_Verfolgung_CV.log'
+logfile = 'logs/Alf_Verfolgung_CV.log'
 logging.basicConfig(filename=logfile,level=logging.DEBUG ,format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
 logging.FileHandler(logfile, mode="w", encoding=None, delay=False)logging.info("----------- Starte Alfred Modul - Verfolgung {0} ---------------".format(Version))
 
