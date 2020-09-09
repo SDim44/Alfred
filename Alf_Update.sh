53903d6135664457295838db4a8c0c4be598afb4
SDim44 <68907465+SDim44@users.noreply.github.com>
2020-09-09 23:00:20 +0200

    Update Alf_Update.sh

--- Alf_Update.sh
+++ Alf_Update.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-lxterminal --title="Alfred Update" --working-directory="/home/pi/Alfred" --command bash -c "cd /home/pi/Alfred & git pull & echo "Alfred ist up-to-date"; bash"
+
+cd /home/pi/Alfred & git pull
+
 sudo chmod +x /home/pi/Alfred
 sudo chmod +x /home/pi/Alfred/Alf_Start.sh
 sudo chmod +x /home/pi/Alfred/Alf_Stop.sh
 sudo chmod +x /home/pi/Alfred/Alf_Main.py
 
-
-
+echo "Alfred ist up-to-date"
