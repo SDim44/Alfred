85ec31a276fdd553c8a3c16fab4233a483425d3d
SDim44 <68907465+SDim44@users.noreply.github.com>
2020-09-14 14:01:49 +0200

    Update Alf_Start.sh

--- Alf_Start.sh
+++ Alf_Start.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+
+# Ladescreen setzten
 echo "99.gif" > emotion.conf
 sleep 2
+
+#Programme starten
 lxterminal --title="Alfred GUI" --working-directory="/home/pi/Alfred" --command bash -c "python2.7 Alf_GUI.py; bash" &
 lxterminal --title="Alfred Speech" --working-directory="/home/pi/Alfred" --command bash -c "python2.7 Alf_Speech.py; bash" &
 lxterminal --title="Alfred Main" --working-directory="/home/pi/Alfred" --command bash -c "sudo python2.7 Alf_Main.py; bash"
