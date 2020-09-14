ce96edc373e01cacb27928c7324ef68eb4c8029c
SDim44 <68907465+SDim44@users.noreply.github.com>
2020-09-14 14:01:55 +0200

    Update Alf_Update.sh

--- Alf_Update.sh
+++ Alf_Update.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Update herunterladen
+cd /home/pi/Alfred & git pull
+
+# Berechtigungen setzten
 sudo chmod +x /home/pi/Alfred
 sudo chmod +x /home/pi/Alfred/Alf_Start.sh
 sudo chmod +x /home/pi/Alfred/Alf_Stop.sh
