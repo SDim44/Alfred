102bfa0e7da423bcded35fc4b5761e822c178804
SDim44 <68907465+SDim44@users.noreply.github.com>
2020-09-14 14:01:51 +0200

    Update Alf_Stop.sh

--- Alf_Stop.sh
+++ Alf_Stop.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-pkill -f Alf_GUI.py
-pkill -f Alf_Speech.py
+
+#Programme beenden
+sudo pkill -f Alf_GUI.py
+sudo pkill -f Alf_Speech.py
 sudo pkill -f Alf_Main.py
+
+#Emotion/Bild setzten
 echo "0.gif" > emotion.conf
