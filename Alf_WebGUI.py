033f0df62ada654c865bcbaadb43ced2a9247568
SDim44 <68907465+SDim44@users.noreply.github.com>
2020-10-24 00:26:45 +0200

    Update Alf_WebGUI.py

--- Alf_WebGUI.py
+++ Alf_WebGUI.py
@@ -10,63 +10,80 @@
 #       Flask Web Server
 
 
-from flask import Flask, render_template
-from flask_socketio import SocketIO, emit
-from flask_cors import CORS
-import math
-from multiprocessing import Process, Queue
-
-_debug = True
-_fulldebug = False
-   
-app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret'
-CORS(app)
-socketio = SocketIO(app)
 
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+
+def run(arg=1):
+    try:
+
+        from flask import Flask, render_template
+        from flask_socketio import SocketIO, emit
+        from flask_cors import CORS
+        import math
+        from multiprocessing import Process, Queue
+
+        _debug = False
+        _fulldebug = False
+        _ret = ''
+        
+        app = Flask(__name__)
+        app.config['SECRET_KEY'] = 'secret'
+        CORS(app)
+        socketio = SocketIO(app)
 
 
-@socketio.on_error_default
-def default_error_handler(e):
-    print("\n\n\t!!!ERROR")
-    print(request.event["message"])
-    print(request.event["args"])
+        @app.route('/')
+        def index():
+            return render_template('index.html')
 
 
-@socketio.on('control', namespace='/control')
-def control(message):
-    data = message["data"]
-    if _fulldebug:
-        print(data)
+        @socketio.on_error_default
+        def default_error_handler(e):
+            print("\n\n\t!!!ERROR")
+            print(request.event["message"])
+            print(request.event["args"])
 
-    if "left" in data.keys():
-        x = data["left"][0]
-        y = data["left"][1]
-        if _debug: 
-            print("[Server] Left: ",x,",",y)
-        #do things with the left joystick
-    elif "right" in data.keys():
-        x = data["right"][0]
-        y = data["right"][1]
-        if _debug: 
-            print("[Server] Right: ",x,",",y)
-        #do things with the right joystick
-    elif "A" in data.keys():
-        if _debug: 
-            print("[Server] A")
-        #Button A
-    elif "B" in data.keys():
-        if _debug: 
-            print("[Server] B")
-        #Button B
-    elif "C" in data.keys():
-        if _debug: 
-            print("[Server] C")
-        #Button C
 
-if __name__ == "__main__":
-    socketio.run(app, host="0.0.0.0", debug=True, use_reloader=False)
+        @socketio.on('control', namespace='/control')
+        def control(message):
+            data = message["data"]
+            if _fulldebug:
+                print(data)
+
+            if "left" in data.keys():
+                x = data["left"][0]
+                y = data["left"][1]
+                if _debug: 
+                    print("[Server] Left: ",x,",",y)
+                #do things with the left joystick
+            elif "right" in data.keys():
+                x = data["right"][0]
+                y = data["right"][1]
+                if _debug: 
+                    print("[Server] Right: ",x,",",y)
+                #do things with the right joystick
+            elif "A" in data.keys():
+                if _debug: 
+                    print("[Server] A")
+                #Button A
+            elif "B" in data.keys():
+                if _debug: 
+                    print("[Server] B")
+                #Button B
+            elif "C" in data.keys():
+                if _debug: 
+                    print("[Server] C")
+                #Button C
+
+        socketio.run(app, host="0.0.0.0", debug=True, use_reloader=False)
+
+        if arg = 0:
+            socketio.switch = False
+    
+    except:
+        _ret = 'Error'
+        return _ret
+
+    except KeyboardInterrupt:
+        _ret = 'Stoped'
+        socketio.stop(app)
