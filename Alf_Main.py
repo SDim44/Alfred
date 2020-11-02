544a5e5128d9e6598d053cc4afbd2c84c9912b0a
SDim44 <68907465+SDim44@users.noreply.github.com>
2020-11-02 23:33:04 +0100

    u

--- Alf_Main.py
+++ Alf_Main.py
@@ -132,8 +132,8 @@ def systemcheck():
             print("in if")
             for act in dev.actuatorlist:
                 print(act.name)
-                act.do(dev.protocol,dev.mac_address,30)
-                act.do(dev.protocol,dev.mac_address,0)
+                act.do(dev.protocol,dev.mac_address,30) ####Funktioniert nicht!!!
+                act.do(dev.protocol,dev.mac_address,0)####Funktioniert nicht!!!
 
 
 
