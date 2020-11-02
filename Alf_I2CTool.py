f0d001c22f3a840cc7e12e0b17acdcf5e4ca3079
SDim44 <68907465+SDim44@users.noreply.github.com>
2020-11-02 23:26:03 +0100

    Update Alf_I2CTool.py

--- Alf_I2CTool.py
+++ Alf_I2CTool.py
@@ -27,8 +27,8 @@ def send(add,cmd):
     add = hex(add)
     cmd = hex(cmd)
 
-    add = int(add,16)
-    cmd = int(cmd,16)
+    #add = int(add,16)
+    #cmd = int(cmd,16)
 
     print(add)
     print(cmd)
